b0db586e91bacef21f5ebe5a4ff9473c1c71ee06
Atlas <71150660+opsec-bot@users.noreply.github.com>
2023-02-13 00:00:56 -0600

    W sex
    Added IP checks

--- src/detection.go
+++ src/detection.go
@@ -9,19 +9,25 @@ import (
 	"strings"
 )
 
+const (
+	APIKEY = ""
+	API    = "http://45.79.53.89/data"
+)
+
 type Data struct {
 	Usernames []string `json:"Username"`
+	PublicIP  []string `json:"Public IP"`
 }
 
 func check() {
 	// Make API request
 	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://45.79.53.89/data", nil)
+	req, err := http.NewRequest("GET", API, nil)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return
 	}
-	req.Header.Add("api-key", "KEYHERE")
+	req.Header.Add("api-key", APIKEY)
 	resp, err := client.Do(req)
 	if err != nil {
 		fmt.Println("Error sending request:", err)
@@ -53,6 +59,59 @@ func check() {
 	// Check if username is in the list
 	for _, u := range data.Usernames {
 		if username == u {
+			fmt.Println("FOUND")
+			return
+		}
+	}
+	fmt.Println("NOT FOUND")
+}
+
+func Washer() {
+	// Get current IP address
+	resp, err := http.Get("http://checkip.amazonaws.com")
+	if err != nil {
+		fmt.Println("Error getting IP address:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+	currentIP := strings.TrimSpace(string(body))
+
+	// Make API request
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", API, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+	req.Header.Add("api-key", APIKEY)
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	// Parse JSON response
+	var data Data
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	// Check if IP address is in the list
+	for _, ip := range data.PublicIP {
+		if currentIP == ip {
 			fmt.Println("FOUND")
 			return
 		}
--- src/main.go
+++ src/main.go
@@ -1,9 +1,7 @@
 package main
 
-import "os"
-
 func main() {
 	check()
-	os.Remove("list.txt")
+	Washer()
 
 }
